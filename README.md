f12a58bcc50601bd09ead0cb346eee1cbda97fb9
Nnenna John <nnennajohn@gmail.com>
2013-06-22 20:02:05 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Sample Rails app
+This is the sample application for
+[*Ruby on Rails Tutorial: Learn Rails by Example*](http://railstutorial.org/)
+by [Nnenna John](http://nnennajohn.com/).
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'd9c2c3bf52346a90b7f1cf108cbd129ff519f5d0a573d6749dd348fdfe2c6e37f8d71dc1055b46ccbbfd3dc98854cb9c80d8517347c2f129f94dff5559302f71'
